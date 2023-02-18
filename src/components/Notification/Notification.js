94ee47e045a84fa8fd93416b585562af441dd421
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-02-18 17:49:44 +0200

    add Notification

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Message } from './Notification.styled.js';
+
+const Notification = ({ message }) => <Message>{message}</Message>;
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default Notification;
---
+++ src/components/Notification/Notification.styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Message = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  color: rgb(157, 28, 28);
+`;
